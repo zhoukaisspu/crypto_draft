7a2dfe14d990d2ea9f7e0c87b8fe8ab07b7778dd
kaizhou <kaizhou@oymotion.com>
2018-06-17 19:47:28 +0800

    Improve sieve of Eratosthenes
    1.replace "std::generator" with "std::iota" to fill the vector

--- src/prime/prime.hpp
+++ src/prime/prime.hpp
@@ -6,7 +6,7 @@
 #include <string>
 #include <algorithm>
 #include <cmath>
-
+#include <numeric>
 namespace prime{
     namespace findPrime{
         std::shared_ptr<std::vector<unsigned int>>
@@ -18,13 +18,8 @@ namespace prime{
 		        throw std::out_of_range("maximum length is :" +
 			        std::to_string(maxLen));
 	        std::shared_ptr<std::vector<unsigned int>> prePrime =
-		    std::make_shared<std::vector<unsigned int>>(len - 2);
-	        struct c_unique {
-		        int current;
-		        c_unique() { current = 1; }
-		        int operator()() { return ++current; }
-	        };
-	        std::generate(prePrime->begin(), prePrime->end(), c_unique());
+		    	std::make_shared<std::vector<unsigned int>>(len - 2);
+			std::iota(prePrime->begin(),prePrime->end(),2);
 	        for (unsigned int i = 2; i <= std::sqrt(len); ++i) {
 		        if ((*prePrime)[i - 2] != 0) {
 			        unsigned int j = i;
